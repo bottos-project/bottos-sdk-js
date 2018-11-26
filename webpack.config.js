023a341b9a80e79f56cf6713cbe726fd72c3503d
WenqiXie <szqq602@163.com>
2018-11-26 16:28:20 +0800

    Change webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const path = require('path')
 
 const config = {
   entry: './index.js',
 
   output: {
     filename: 'bottos-sdk-js.min.js',
+    path: path.resolve(__dirname, 'umd'),
     library: 'BottosWalletSDK',
     libraryTarget: 'umd'
   }
